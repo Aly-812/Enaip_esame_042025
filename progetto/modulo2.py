d23baaf2171d708fe5c0b4652822e87c4fde8d54
Aly-812 <alice.casini.93@hotmail.it>
2025-04-07 20:16:05 +0200

    aggiunta funzione modulo 2

--- progetto/modulo2.py
+++ progetto/modulo2.py
@@ -8,12 +8,23 @@ def leggi_da_file(filepath):
     except FileNotFoundError:
         return None
 
-def processa_dati(dati):
+def processa_dati(dati:list) -> list:
     """
     Questa funzione dovrebbe processare una lista di dizionari.
     Implementa la logica per filtrare i dizionari che hanno una chiave 'attivo'
     con valore True e restituire una nuova lista contenente solo i valori
     della chiave 'id' di questi dizionari.
     """
-    # TODO: Implementare la logica di processamento dei dati
-    pass
+    lista_risultati = []
+
+    for diz in dati:
+        if diz["attivo"] == True:
+            lista_risultati.append(diz["id"])
+    return lista_risultati
+
+if __name__ == "__main__":
+    dizionario = leggi_da_file("../dati/test.json")
+    attivi = processa_dati(dizionario)
+    print(attivi)
+
+    # return [item['id'] for item in dati if item.get('attivo') is True]
